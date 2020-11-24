21630f5784a54a996a7ceb0e9e03ab33bd5db942
Mark <markandrewhamm@gmail.com>
2020-11-23 20:27:58 -0600

    meme generator basics working

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./style.css"
 
 class MemeGenerator extends React.Component {
   constructor() {
@@ -6,14 +7,63 @@ class MemeGenerator extends React.Component {
     this.state = {
       topText: "",
       bottomText: "",
-      randomImg: "http://i.imgflip.com/1bij.jpg"
+      randomImg: "http://i.imgflip.com/1bij.jpg",
+      allMemeImgs: []
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.pullImg = this.pullImg.bind(this)
+  }
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(response => {
+        const { memes } = response.data
+        this.setState({
+          allMemeImgs: memes
+        })
+      })
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target
+    this.setState({ [name]: value })
+  }
+
+  pullImg(event) {
+    event.preventDefault()
+    const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
+    this.setState({
+      randomImg: this.state.allMemeImgs[randNum].url
+    })
   }
 
   render() {
     return (
       <div>
-        <h2>stuff</h2>
+        <form className="meme-form" onSubmit={this.pullImg}>
+          <input
+            type="text"
+            name="topText"
+            placeholder="Top Text"
+            value={this.state.topText}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="bottomText"
+            placeholder="Bottom Text"
+            value={this.state.bottomText}
+            onChange={this.handleChange}
+          />
+          <button>Gen</button>
+
+        </form>
+        <div className="meme">
+          <img src={this.state.randomImg} alt="" />
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.bottomText}</h2>
+        </div>
       </div>
     )
   }
