8ea673776619f28d653cd524dcbf3df078821e20
Pol-S <plsbnjy@gmail.com>
2020-12-11 22:00:36 -0600

    Cleaned up the head

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-import React from "react";
-import Header from "./Header";
-import MemeGenerator from "./MemeGenerator";
-=======
 import React from "react"
 import Header from "./Header"
 import MemeGenerator from "./MemeGenerator"
 import Memes from "./Memes"
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
->>>>>>> 5cc089bc3c4e87662c84029b2aba0b33a468302e
 
 function App() {
   return (
