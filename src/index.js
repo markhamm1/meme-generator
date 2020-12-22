74a0114478ca1a131b9b45327736b736ac3f27ea
Mark <markandrewhamm@gmail.com>
2020-12-21 20:43:02 -0600

    frontend authorization and push routes added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import axios from "axios";
+
+var jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
