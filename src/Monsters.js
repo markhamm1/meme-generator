8616dc5b9b22cccc1f423f13aeff7428ba2b3b14
Pol-S <plsbnjy@gmail.com>
2021-01-11 20:31:16 -0600

    Added a canvas and images over absolute images and got body parts to line up. Still need to get entrees to not overlap

--- src/Monsters.js
+++ src/Monsters.js
@@ -28,6 +28,10 @@ class Monsters extends React.Component {
       })
   }
 
+  printMonster(){
+    console.log("logging attempt to save");
+  }
+
 
   render() {
     return (
@@ -36,11 +40,13 @@ class Monsters extends React.Component {
           {this.state.allMemes.map((meme, i) => <li className="index" key={meme.id}>
             <div className="meme">
               <br></br>
-              <img src={meme.head_url} alt="" />
-              <img src={meme.body_url} alt="" />
-              <img src={meme.leg_url} alt="" />
+              <canvas id="monster_canvas" ></canvas>
+              <img src={meme.head_url} alt="" class="monster_head"/>
+              <img src={meme.body_url} alt="" class="monster_body"/>
+              <img src={meme.leg_url} alt="" class="monster_leg"/>
               <h2 className="memes-top">{meme.top_text}</h2>
               <h2 className="memes-bottom">{meme.bottom_text}</h2>
+              <br></br>
               <br></br>
               <Link
                 className="meme-link"
@@ -48,7 +54,11 @@ class Monsters extends React.Component {
                   pathname: `/editmonster/${meme.id}`,
                 }}
               >
-                Edit Meme
+                Edit Monster
+              </Link>
+              <br></br>
+              <Link className="meme-link ">
+                Print Monster
               </Link>
               <br></br>
               <hr></hr>
